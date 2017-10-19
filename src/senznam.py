e9282f3d2406dab0f039f6924844e4219d9a1334
Marrek Nožka <nozka@spseol.cz>
2017-10-19 14:11:45 +0200

    jak se pracuje se senzamem...

--- src/senznam.py
+++ src/senznam.py
@@ -18,3 +18,44 @@ for i in 0, 1, 2, 3, 4, 5, 6, 7, 8, 9:
     
 for _ in range(10):
     print(random.randint(0,100))
+    
+
+seznam = []
+for _ in range(100):
+    seznam.append(random.randint(0,100))
+print(seznam)
+
+# krátce
+sude_mocniny = [i*i for i in range(30) if i % 2 == 0]
+# dlouze
+sude_mocniny = []
+for i in range(30):
+    if i % 2 == 0:
+        sude_mocniny.append(i*i)
+print(sude_mocniny)
+
+length = len
+# Aritmetický průměr
+soucet = 0
+for cislo in seznam:
+    soucet += cislo
+print(soucet / len(seznam))
+
+print(sum(seznam)/len(seznam))
+
+"""
+udělám si seznam náhodných  čísel a vypíšu 
+ty co jsou dělitelné 7
+"""
+
+seznam = [random.randint(0,100) for _ in range(100)]
+for i in seznam:
+    if i % 7 == 0:
+        print(i)
+        
+
+
+
+
+
+
